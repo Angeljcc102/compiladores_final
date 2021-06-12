09d166dbd8a67d58620831bbf440d95d768bd8a0
Angeljcc102 <angeljcc102@outlook.es>
2021-06-12 13:28:09 -0500

    Cambios en funciones de sintaxis para utilizar el analizador semantico

---
+++ semantico.py
@@ -0,0 +1,695 @@
+
+counter = 0
+
+text = " "
+
+
+def increaseCount():
+    global counter
+    counter += 1
+    return "%d" % counter
+
+
+class Node():
+    pass
+
+
+class NULL(Node):
+    def __init__(self):
+        self.n
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + 'null\n'
+
+        return id
+
+
+class intDecl(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class floatDecl(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class stringDecl(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class intAssignmentList(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class floatAssignmentList(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class stringAssignmentList(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class identList(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statement1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statement2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statement3(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statement4(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statement5(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class statementList(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class condition(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation3(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation4(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation5(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation6(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class relation7(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class expression1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class expression2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class expression3(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class expression4(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class expression5(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class addOp1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class addOp2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class multOp1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class multOp2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class terms1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class terms2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class terms3(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class factor1(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class factor2(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class factor3(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class factor4(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class factor5(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class empty(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def translate(self):
+        global text
+        id = increaseCount()
+
+        return id
+
+
+class INT(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class ID(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class PLUS(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class MINUS(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class TIMES(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class DIVIDE(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class ASSIGN(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class EQUALS(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class NE(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class GT(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class LT(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class GE(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class LE(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class INT(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class FLOAT(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
+
+
+class STRING(Node):
+    def __init__(self, name):
+        self.name = name
+
+    def printer(self, ident):
+        print(ident + str(self.name))
+
+    def translate(self):
+        global text
+        id = increaseCount()
+        text += id + str(self.name) + "\n"
+
+        return id
--- sintactico.py
+++ sintactico.py
@@ -1,3 +1,5 @@
+from semantico import *
+
 import ply.ply.yacc as yacc
 
 import codecs
@@ -22,50 +24,64 @@ precedence = (
 def p_intDecl(p):
     '''intDecl : INT intAssignmentList '''
     print("intDecl")
+    p[0] = intDecl(INT(p[1]), intAssignmentList(p[2]), "intDecl")
 
 
 def p_floatDecl(p):
     '''floatDecl : FLOAT floatAssignmentList '''
     print("floatDecl")
+    p[0] = floatDecl(FLOAT(p[1]), floatAssignmentList(p[2]), "floatDecl")
 
 
 def p_stringDecl(p):
     '''stringDecl : STRING stringAssignmentList '''
     print("stringDecl")
+    p[0] = stringDecl(STRING(p[1]), stringAssignmentList(p[2]), "stringDecl")
 
 
 def p_intAssignmentList(p):
     '''intAssignmentList : ID ASSIGN INT SEMICOLON'''
     print("intAssignmentList1")
+    p[0] = intAssignmentList(ID(p[1]), ASSIGN(
+        p[2]), INT(p[3]), "intAssignmentList")
 
 
 def p_floatAssignmentList(p):
     '''floatAssignmentList : ID ASSIGN FLOAT SEMICOLON'''
     print("floatAssignmentList1")
+    p[0] = floatAssignmentList(ID(p[1]), ASSIGN(
+        p[2]), FLOAT(p[3]), "floatAssignmentList")
 
 
 def p_stringAssignmentList(p):
     '''stringAssignmentList : ID ASSIGN STRING SEMICOLON'''
+    print("stringAssignmentList")
+    p[0] = stringAssignmentList(ID(p[1]), ASSIGN(
+        p[2]), INT(p[3]), "stringAssignmentList")
 
 
 def p_identList(p):
     '''identList : ID '''
     print("identList")
+    p[0] = identList(ID(p[1]), "identList")
 
 
 def p_statement1(p):
     '''statement : IF LPAREN condition RPAREN LBRACE statement RBRACE'''
     print("statemen1")
+    p[0] = statement1(p[3], p[6], "statement1")
 
 
 def p_statement2(p):
     '''statement : WHILE LPAREN condition RPAREN LBRACE statement RBRACE'''
     print("statement2")
+    p[0] = statement2(p[3], p[6], "statement2")
 
 
 def p_statement3(p):
     '''statement : DO LBRACE statement RBRACE WHILE LPAREN condition RPAREN SEMICOLON'''
     print("statement3")
+    p[0] = statement3(p[3], p[7], "statement3")
 
 
 """
@@ -78,136 +94,169 @@ def p_statement3(p):
 def p_statement4(p):
     '''statement : IF LPAREN condition RPAREN LBRACE statement RBRACE ELSE LBRACE statement RBRACE'''
     print("statement4")
+    p[0] = statement4(p[3], p[6], p[10], "statement4")
 
 
-def p_statement5(p):
+def p_statementEmpty(p):
     '''statement : empty'''
     print("empty")
+    p[0] = NULL()
 
 
 def p_statementList(p):
     '''statement : statement'''
     print("statementList")
+    p[0] = statementList(p[1], "statementList")
 
 
 def p_condition(p):
     '''condition : expression relation expression'''
     print("condition")
+    p[0] = condition(p[1], p[2], p[3], condition)
 
 
 def p_relation1(p):
     '''relation : ASSIGN'''
     print("relation1")
+    p[0] = relation1(ASSIGN(p[1]), "relation1")
 
 
 def p_relation2(p):
     '''relation : EQUALS '''
     print("relation2")
+    p[0] = relation2(EQUALS(p[1]), "relation2")
 
 
 def p_relation3(p):
     '''relation : NE'''
     print("relation3")
+    p[0] = relation3(NE(p[1]), "relation3")
 
 
 def p_relation4(p):
     '''relation : GT'''
     print("relation4")
+    p[0] = relation4(GT(p[1]), "relation4")
 
 
 def p_relation5(p):
     '''relation : LT'''
     print("relation5")
+    p[0] = relation5(LT(p[1]), "relation5")
 
 
 def p_relation6(p):
     '''relation : GE'''
     print("relation6")
+    p[0] = relation6(GE(p[1]), "relation6")
 
 
 def p_relation7(p):
     '''relation : LE'''
     print("relation7")
+    p[0] = relation7(LE(p[1]), "relation7")
 
 
 def p_expression1(p):
     '''expression : terms'''
     print("expression1")
+    p[0] = expression1(p[1], "expression1")
 
 
 def p_expression2(p):
     '''expression : addOp terms'''
     print("expression2")
+    p[0] = expression2(p[1], p[2], "expression2")
 
 
 def p_expression3(p):
     '''expression : multOp terms'''
     print("expression3")
+    p[0] = expression3(p[1], p[2], "expression3")
 
 
-def p_expression3(p):
+def p_expression4(p):
     '''expression : expression addOp terms'''
-    print("expression3")
+    print("expression4")
+    p[0] = expression4(p[1], p[2], p[3], "expression4")
+
+
+def p_expression5(p):
+    '''expression : expression multOp terms'''
+    print("expression5")
+    p[0] = expression5(p[1], p[2], p[3], "expression5")
 
 
 def p_addOp1(p):
     '''addOp : PLUS'''
     print("addOp1")
+    p[0] = addOp1(PLUS(p[0]), "addOp1")
 
 
 def p_addOp2(p):
     '''addOp : MINUS'''
     print("addOp2")
+    p[0] = addOp2(MINUS(p[0]), "addOp2")
 
 
 def p_multOp1(p):
     '''multOp : TIMES'''
     print("multOp1")
+    p[0] = multOp1(TIMES(p[0]), "multOp1")
 
 
 def p_multOp2(p):
     '''multOp : DIVIDE'''
     print("multOp2")
+    p[0] = multOp2(DIVIDE(p[0]), "multOp2")
 
 
 def p_terms1(p):
     '''terms : factor'''
     print("terms1")
+    p[0] = terms1(p[1], "terms1")
 
 
 def p_terms2(p):
     '''terms : terms addOp factor'''
     print("terms2")
+    p[0] = terms2(p[1], p[2], p[3], "terms2")
 
 
 def p_terms3(p):
     '''terms : terms multOp factor'''
     print("terms3")
+    p[0] = terms3(p[1], p[2], p[3], "terms3")
 
 
 def p_factor1(p):
     '''factor : ID'''
     print("factor1")
+    p[0] = factor1(ID(p[1]), "factor1")
 
 
 def p_factor2(p):
     '''factor : INT'''
     print("factor2")
+    p[0] = factor2(INT(p[1]), "factor2")
 
 
 def p_factor3(p):
     '''factor : FLOAT'''
     print("factor3")
+    p[0] = factor3(FLOAT(p[1]), "factor3")
 
 
 def p_factor4(p):
     '''factor : STRING'''
     print("factor4")
+    p[0] = factor4(STRING(p[1]), "factor4")
 
 
 def p_factor5(p):
     '''factor : LPAREN expression RPAREN'''
     print("factor5")
+    p[0] = factor5(p[2], "factor5")
 
 
 def p_empty(p):
@@ -234,6 +283,12 @@ filename.close()
 
 resultados = parser.parse(texto)
 
-newfile = codecs.open("lex.txt", "w", "utf-8")
+#resultados.printer(" ")
+# print(result.translate())
+
+
+newfile = codecs.open("tree.txt", "w", "utf-8")
+newfile.wirte(resultados.translate())
+newfile.close()
 
 print(resultados)
